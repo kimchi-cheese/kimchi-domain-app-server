f8b9d4642a88daab67ce3111bcd9eec317ba7398
창민 <ckd29672@naver.com>
2023-07-20 11:38:42 +0900

    chore: eureka client 설정 및 연동

--- src/main/java/com/kimcheese/kimchidomainappserver/KimchiDomainAppServerApplication.java
+++ src/main/java/com/kimcheese/kimchidomainappserver/KimchiDomainAppServerApplication.java
@@ -2,12 +2,23 @@ package com.kimcheese.kimchidomainappserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class KimchiDomainAppServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KimchiDomainAppServerApplication.class, args);
 	}
 
+	@GetMapping("/domain/info")
+	public ResponseEntity<String> info(){
+		return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+	}
 }
